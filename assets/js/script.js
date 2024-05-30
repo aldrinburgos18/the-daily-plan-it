ac2229ca87114182ec8cfe21cf3c821177f8578f
Aldrin Burgos <burgos.aldrin@gmail.com>
2024-05-30 17:56:29 -0500

    dynamically create rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-var currentDay = moment(new Date()).format("dddd, MMMM Do YYYY");
+var currentDay = moment(new Date()).format("dddd, MMMM Do");
 var currentTime = moment(new Date()).format("HH:mm:ss A");
 
-console.log(currentDay, currentTime);
+$("#currentDay").html("Today is " + "<strong>" + currentDay + "</strong>");
+
+
+//dynamically create rows
+var createRows = function () {
+  for (var i = 9; i < 19; i++) {
+    $("#task-container").append(
+      `<div class="row" id="task-${i-9}">
+        <div class="col-2 hour">${(i<=11) ? i +'AM' : ( (i === 12) ? i + 'PM' : i-12+'PM' )}</div>
+        <div class="col-9 border border-dark border-bottom-0 position-relative">
+          <p><input class="task-input" /></p>
+        </div>
+        <div class="col-1 border border-dark border-left-0 rounded-right save-btn">
+          <span class="oi h1 m-0" data-glyph="check"></span>
+        </div>
+      </div>`
+    );
+  }
+};
+
+createRows();
